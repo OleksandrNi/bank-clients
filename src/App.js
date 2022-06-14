f087e4c9edd7a5383b05877a4d91da1ecd107497
Alexandr Nikytenko <nika200382@gmail.com>
2022-06-14 19:06:36 +0300

    training-tools

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import { fetchCusomers } from './asyncAction/customers';
+import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash.cash);
+  const customers = useSelector(state => state.customers.customers);
+
+  const addCash = (cash) => {
+    dispatch({type:'ADD_CASH', payload: cash})
+  }
+
+  const getCash = (cash) => {
+    dispatch({type:'GET_CASH', payload: cash})
+  }
+
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now(),
+    }
+    dispatch(addCustomerAction(customer))
+  }
+
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{display:"flex"}}>
+        <div style={{fontSize:"2rem", marginTop:"20px"}}>{cash}</div>
+        <button onClick={() => addCash(Number(prompt()))}>ADD money</button>
+        <button onClick={() => getCash(Number(prompt()))}>GET money</button>
+        <button onClick={() => addCustomer(prompt())}>Add customer</button>
+        <button onClick={() => dispatch(fetchCusomers())}>Add customer from base</button>
+      </div>
+      {customers.length > 0 ?
+        <div>
+          {customers.map(customer =>
+            <div onClick={() => removeCustomer(customer)} style={{fontSize:"2rem", marginTop:"20px"}}>{customer.name}</div>
+          )}
+        </div>
+        : 
+        <div style={{fontSize:"2rem", marginTop:"20px"}}>
+          No clients
+        </div>
+      }
     </div>
   );
 }
